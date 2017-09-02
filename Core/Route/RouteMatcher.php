e5964d5ec643d0708c7cd8a2f0f6abe58d85d37f
Codimiracle <codimiracle@outlook.com>
2017-09-02 15:53:07 +0800

    RouteMatcher: Route Regular Matcher.

--- Core/Route/RouteMatcher.php
+++ Core/Route/RouteMatcher.php
@@ -8,11 +8,23 @@
 
 namespace Core\Route;
 
-
+/**
+ * Interface RouteMatcher
+ * It provides simple route for URL
+ */
 interface RouteMatcher
 {
+
     /**
+     * Get the Route Name.
+     * @return string
+     */
+    public function getName();
+    /**
+     * Match Route Ruler.
+     * @param NodePath $node_path
      * @return NodePath|null
      */
     public function match($node_path);
+
 }
