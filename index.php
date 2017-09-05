84db2242b3437a296cfbf53a6ec3d423c057a8aa
Codimiracle <codimiracle@outlook.com>
2017-09-05 14:06:20 +0800

    index.php: generate kernel instance.

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ require_once "Core/Kernel.php";
 
 use Core\Kernel;
 
-$kenel = new \Core\Kernel();
-$router = $kenel->getRouter();
-$router->route();
+function __autoload($class_name)
+{
+    $path = explode("\\",$class_name);
+    $class_path = join("/",$path);
+    require_once $class_path.".php";
+}
+
+$kernel = new Kernel();
+$kernel->launch();
