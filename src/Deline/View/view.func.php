654cc3a77683f9b189461e38b070dd83735a5641
Codimiracle <Codimiracle@outlook.com>
2018-04-10 22:54:30 +0800

    fixed deline_show_template function.

--- src/Deline/View/view.func.php
+++ src/Deline/View/view.func.php
@@ -27,21 +27,21 @@ function deline_show_file($filename) {
     $session    = $GLOBALS["session"];
     require_once $filename;
 }
-function delien_show_template($template_name) {
+function deline_show_template($template_name) {
     deline_show_file(getcwd()."/templates/tpl.".$template_name.".php");
 }
 function deline_show_html_head() {
-    delien_show_template("html.head");
+    deline_show_template("html.head");
 }
 function deline_show_html_foot() {
-    delien_show_template("html.foot");
+    deline_show_template("html.foot");
 }
 function deline_show_header() {
-    delien_show_template("header");
+    deline_show_template("header");
 }
 
 function deline_show_footer() {
-    delien_show_template("footer");
+    deline_show_template("footer");
 }
 
 function deline_show_text($text) {
