a64bb31b87866c789a9af699b7c24b05fcea1d32
Codimiracle <Codimiracle@outlook.com>
2018-05-15 13:02:28 +0800

    deline project update 2018-05-15 13:02

---
+++ src/Deline/Model/DAO/DecrementPager.php
@@ -0,0 +1,42 @@
+<?php
+namespace Deline\Model\DAO;
+
+class DecrementPager implements Pager
+{
+
+    private $offset;
+
+    private $length;
+
+    private $key;
+
+    /**
+     *
+     * @param string $key
+     *              NOTE: you must escape the $key
+     * @param int $offset
+     * @param int $length
+     */
+    public function __construct($key, $offset, $length)
+    {
+        $this->offset = $offset;
+        $this->length = $length;
+        $this->key = $key;
+    }
+
+    public function getTranformSQL($sentence)
+    {
+        return $sentence . "ORDER BY " . $this->key . " LIMIT ?, ?";
+    }
+
+    public function getOffset()
+    {
+        return $this->offset * $this->length;
+    }
+
+    public function getLength()
+    {
+        return $this->length;
+    }
+}
+
---
+++ src/Deline/Model/DAO/SimplePager.php
@@ -0,0 +1,32 @@
+<?php
+namespace Deline\Model\DAO;
+
+class SimplePager implements Pager
+{
+
+    private $offset;
+
+    private $length;
+
+    public function __construct($offset, $length)
+    {
+        $this->offset = $offset;
+        $this->length = $length;
+    }
+
+    public function getTranformSQL($sentence)
+    {
+        return $sentence . " LIMIT ?, ?";
+    }
+
+    public function getOffset()
+    {
+        return $this->offset * $this->length;
+    }
+
+    public function getLength()
+    {
+        return $this->length;
+    }
+}
+
